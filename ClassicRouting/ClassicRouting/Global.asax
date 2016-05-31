03c0f0a4a7be3d45c891dfecc2caa4024eaf9f54
S. Marcelo Volta <marcelovolta@gmail.com>
2016-05-31 09:49:26 -0300

    Proyecto Inicial

---
+++ ClassicRouting/ClassicRouting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClassicRouting.MvcApplication" Language="C#" %>
---
+++ ClassicRouting/ClassicRouting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClassicRouting.Startup))]
+namespace ClassicRouting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
