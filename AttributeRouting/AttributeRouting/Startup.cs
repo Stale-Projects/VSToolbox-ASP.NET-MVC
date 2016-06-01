b53e05d7000d5021c84bb4763574f2830ee858b0
S. Marcelo Volta <marcelovolta@gmail.com>
2016-06-01 09:59:52 -0300

    Proyecto Inicial

---
+++ AttributeRouting/AttributeRouting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AttributeRouting.Startup))]
+namespace AttributeRouting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
